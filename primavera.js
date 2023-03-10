a361c120c77203f7677413e9442164668f6d0f26
ski <108815730+thelegendski@users.noreply.github.com>
2023-03-10 08:52:38 -0500

    Create

---
+++ primavera.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const { Client } = require("@bhavjit/khan-api")
+const client = new Client()
+
+async function main () {
+  const replies = await client.getAllMessageReplies("ag5zfmtoYW4tYWNhZGVteXJBCxIIVXNlckRhdGEiHmthaWRfMTAzNzkwNDA4MTM5MTE4NzA4MDQ5ODUwNwwLEghGZWVkYmFjaxiAgJPzlcmnCAw")
+  const contestants = []
+  replies.forEach(reply => {
+    let string
+    try {
+      string = JSON.parse(reply.text.match(/\[.+\]/))
+    }
+    catch(e){}
+    if(string)contestants.push(`"${string[0]}"`)
+  })
+  fs.writeFile('./primaveraContestants.js', `var contestants = [${contestants}]`, err => {
+    if(err) console.error(err)
+    else console.log("file has been updated.")
+  })
+}
+main()
