3d846f47a11d0f17976407aa471769b2bed1a197
ski <108815730+thelegendski@users.noreply.github.com>
2023-03-10 21:58:40 -0500

    Update primavera.js

--- primavera.js
+++ primavera.js
@@ -5,13 +5,16 @@ const client = new Client()
 
 async function main() {
     const replies = await client.getAllMessageReplies("ag5zfmtoYW4tYWNhZGVteXJBCxIIVXNlckRhdGEiHmthaWRfMTAzNzkwNDA4MTM5MTE4NzA4MDQ5ODUwNwwLEghGZWVkYmFjaxiAgJOjuLmJCww")
-    const contestants = [], blacklist = [], regex = /\$\[\s*("|')[a-zA-Z0-9 ]+("|')\s*,\s*[0-9]\s*,\s*("|')[A-Za-z0-9 ]+("|')\s*\]\$/
+    const contestants = [], blacklist = [], teams = ["ninjaz", "astronomers", "leviathans"], regex = {
+        join: /\$\[\s*("|')[a-zA-Z0-9 ]+("|')\s*,\s*[0-9]\s*,\s*("|')[A-Za-z0-9 ]+("|')\s*\]\$/,
+        username: /(;|function|=>|\/\/|"|'|`)/,
+    }
     let comments = ''
     replies.forEach(reply => {
         let string
         try {
-            if(!blacklist.includes(reply.author.kaid)){
-                if(regex.test(reply.text)) string = JSON.parse(reply.text.match(/\[.+\]/))
+            if(!blacklist.includes(reply.author.kaid) && !regex.username.test(reply.author.nickname)){
+                if(regex.join.test(reply.text)) string = JSON.parse(reply.text.match(/\[.+\]/))
                 else comments += `//WARNING! join failed: ${reply.author.nickname} | ${reply.author.kaid}\n`
             }
             else comments += `//WARNING! blacklisted user attempted to join: ${reply.author.nickname} | ${reply.author.kaid}\n`
@@ -21,8 +24,13 @@ async function main() {
             console.error(e) 
         }
         if (string) {
-            contestants.push(`"${string[0]}"`)
-            console.log(`${reply.author.nickname} joined successfully.`)
+            if(teams.includes(string[2])){
+                contestants.push(`{name: "${reply.author.nickname}", kaid: "${reply.author.kaid}", level: ${Math.min(Math.max(string[1], 0), 3)}, team: "${string[2]}"}`)
+                console.log(`${reply.author.nickname} joined successfully.`)
+            }
+            else {
+                comments += `//WARNING! invalid join code [team]: ${reply.author.nickname} | ${reply.author.kaid} | ${string[2]}\n`
+            }
         }
     })
     fs.writeFile('./primaveraContestants.js', `var contestants = [${contestants}]\n${comments}`, err => {
