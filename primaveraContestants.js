cb5d66731289e931654e885741afeb5eb945d577
thelegendski <thelegendski@users.noreply.github.com>
2023-03-11 02:20:14 +0000

    update primavera contestants

--- primaveraContestants.js
+++ primaveraContestants.js
@@ -1,1 +1,2 @@
-var contestants = ["ski","DR","Astro","rick astley"]
+var contestants = ["ski","DR","Astro","rick astley","Limelight"]
+//WARNING! join failed: Astro | kaid_4391100538307580650220212
