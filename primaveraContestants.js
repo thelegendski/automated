447c17f3f4f8cdb6ba4af432588a1eacadadfc4f
thelegendski <thelegendski@users.noreply.github.com>
2023-03-11 03:03:26 +0000

    update primavera contestants

--- primaveraContestants.js
+++ primaveraContestants.js
@@ -1,2 +1,3 @@
-var contestants = ["ski","DR","Astro","rick astley","Limelight"]
+var contestants = [{name: "ski", kaid: "kaid_1037904081391187080498507", level: 3, team: "ninjaz"},{name: "Dominic R.", kaid: "kaid_152917881906477482126765", level: 2, team: "astronomers"},{name: "Astro", kaid: "kaid_4391100538307580650220212", level: 3, team: "astronomers"},{name: "Lemon Games 🦈", kaid: "kaid_317222614333275516765464", level: 2, team: "leviathans"}]
+//WARNING! invalid join code [team]: S. M. V. | kaid_266891459920003908397632 | i want cake
 //WARNING! join failed: Astro | kaid_4391100538307580650220212
