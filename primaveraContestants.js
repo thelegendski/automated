6a4625373782c53074e98a04dd0324191038da5c
ski <thelegendski@proton.me>
2023-03-15 11:18:35 -0400

    update from GAS at Wed Mar 15 2023 11:18:34 GMT-0400 (Eastern Daylight Time)

--- primaveraContestants.js
+++ primaveraContestants.js
@@ -1,6 +1,6 @@
 var contestants = [{name: "ski", kaid: "kaid_1037904081391187080498507", level: 3, team: "ninjaz"},{name: "Dominic R.", kaid: "kaid_152917881906477482126765", level: 2, team: "astronomers"},{name: "Astro", kaid: "kaid_4391100538307580650220212", level: 3, team: "astronomers"},{name: "Lemon Games ?", kaid: "kaid_317222614333275516765464", level: 2, team: "leviathans"},{name: "ski", kaid: "kaid_1037904081391187080498507", level: 1, team: "ninjaz"},{name: "ski", kaid: "kaid_1037904081391187080498507", level: 2, team: "astronomers"},{name: "Liam K.", kaid: "kaid_160653079463992085090428", level: 3, team: "Ex-Host"}]
 //WARNING! invalid join code [team]: S. M. V. | kaid_266891459920003908397632 | i want cake
 //WARNING! join failed: Astro | kaid_4391100538307580650220212
-//WARNING! join failed: Lemon Games ? | kaid_317222614333275516765464
+//WARNING! invalid join code [team]: Lemon Games ? | kaid_317222614333275516765464 | Founder
 //WARNING! join failed: VVhiteTiger | kaid_154605635905555420140323
-//WARNING! join failed: ski | kaid_1037904081391187080498507
+//WARNING! invalid join code [team]: ski | kaid_1037904081391187080498507 | team ski
