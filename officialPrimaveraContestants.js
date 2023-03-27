c86065169307d5c2fe87c1daec0b6349c4a4932d
ski <thelegendski@proton.me>
2023-03-27 17:05:33 -0400

    update from GAS at Mon Mar 27 2023 17:05:32 GMT-0400 (Eastern Daylight Time)

--- officialPrimaveraContestants.js
+++ officialPrimaveraContestants.js
@@ -1,1 +1,1 @@
-var contestants = [{name: "ski", kaid: "https://khanacademy.org/profile/kaid_1037904081391187080498507", level: 4, team: "magus"}]
+var contestants = [{name: "Crystal Coding", kaid: "https://khanacademy.org/profile/kaid_351465532815782433620675", level: 0, team: "Altum"},{name: "The No-Sock Guy(alt)", kaid: "https://khanacademy.org/profile/kaid_888430478984465976164714", level: 3, team: "Magus"},{name: "G???x?R????? ?", kaid: "https://khanacademy.org/profile/kaid_574576985099042639713735", level: 1, team: "Aequor"}]
