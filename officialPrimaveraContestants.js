f19f7b0f46c3674a246b0bf26a66c514a9fb5bf8
ski <thelegendski@proton.me>
2023-03-30 14:22:12 -0400

    update from GAS at Thu Mar 30 2023 14:22:11 GMT-0400 (Eastern Daylight Time)

--- officialPrimaveraContestants.js
+++ officialPrimaveraContestants.js
@@ -4,3 +4,4 @@ var contestants = [{name: "Crystal Coding # GO Team ALTUM", kaid: "https://khana
 //WARNING! join failed: Invisible Bob | kaid_4357452902529547972393951
 //WARNING! join failed: ???????? {??????} | kaid_4408766809036862538405271
 //WARNING! join failed: FunkyStone | kaid_100883727649169325640401
+//WARNING! invalid join code [team]: Noah H. | kaid_1121453835893982129568101 | Opus
